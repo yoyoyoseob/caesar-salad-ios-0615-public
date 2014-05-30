0ffc136a36441cdf50670fb76c334aa6edd4281c
Chris Gonzales <gonzales.chris.a@gmail.com>
2014-05-30 16:30:19 -0400

    project setup and tests, the whole enchilada

---
+++ CaesarCipher/CaesarCipher/FISCaesarCipher.h
@@ -0,0 +1,15 @@
+//
+//  FISCaesarCipher.h
+//  CaesarCipher
+//
+//  Created by Chris Gonzales on 5/29/14.
+//  Copyright (c) 2014 FIS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FISCaesarCipher : NSObject
+//declare methods here
+
+
+@end
